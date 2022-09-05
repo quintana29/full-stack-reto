97e337c3e93a6b2fe8e4dd29d49de3241e9c5c42
quintana29 <jhonatanquintana29@gmail.com>
2022-09-04 22:40:09 -0500

    Completando proyecto, agregando modulo busisness y aplication

--- business/src/main/java/org/example/business/gateway/ConsultaCartasMaestrasService.java
+++
@@ -1,9 +0,0 @@
-package org.example.business.gateway;
-
-import org.example.business.gateway.model.CartaMaestra;
-import reactor.core.publisher.Flux;
-
-public interface ConsultaCartasMaestrasService {
-
-    Flux<CartaMaestra> consultarTodasLasCartas();
-}
--- domain/src/main/java/org/example/domain/values/TableroId.java
+++
@@ -1,6 +0,0 @@
-package org.example.domain.values;
-
-import co.com.sofka.domain.generic.Identity;
-
-public class TableroId extends Identity {
-}
